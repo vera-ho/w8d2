10195060f25898b5e94d2da962915d64dd311f74
“Evan <frommere@uchicago.edu>
2022-05-17 15:15:19 -0400

    phase 2 complete

---
+++ intro_js_skeleton/phase_2_enumerable.js
@@ -0,0 +1,26 @@
+Array.prototype.myEach = function(callback) {
+    for(let i=0; i<this.length; i++) {
+        callback(this[i]);
+    }
+
+}
+
+function times_two(x) {
+    return x*2;
+}
+
+Array.prototype.myMap = function(callback) {
+    let array = []
+    for(let i=0; i<this.length; i++) {
+        array.push(callback(this[i]));
+    }
+    return array;
+}
+
+Array.prototype.myReduce = function(callback, initialValue=0) {
+    let acc = initialValue || 0;
+    for(let i=0; i<this.length; i++) {
+        acc += callback(this[i])
+    }
+    return acc
+}
