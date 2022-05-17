a082c9deb0301bdb82bbd1d8261376fcf4b4a49e
“Evan <frommere@uchicago.edu>
2022-05-17 16:43:59 -0400

    phase 4

--- intro_js_skeleton/phase_4_recursion.js
+++ intro_js_skeleton/phase_4_recursion.js
@@ -9,3 +9,60 @@ function range(start, end) {
 }
 
 // console.log(range(1, 10));
+
+function sumRecc(arr) {
+    if(arr.length === 1) {
+        return arr[0];
+    }
+    if(arr.length ===0) {
+        return 0;
+    }
+    let sum = arr[0] + sumRecc(arr.slice(1,arr.length))
+    return sum;
+}
+
+function exponent_version1(base, exp) {
+    return (base**(exp))
+}
+function exponent_version2(base, exp) {
+    if (exp ===1) {
+        return base;
+    }
+    if (exp===0) {
+        return 1;
+    }
+    let result = 0
+    if (exp%2===0) {
+        return base**(exp/2)**2;
+    }
+    else{
+        result = base*(exponent_version2(base, (exp-1)/2)**2);
+        return result;
+    }
+}
+
+function fibonacci(n) {
+    if (n===1) {
+        return [1];
+    }
+    if (n===2) {
+        return [1,1];
+    }
+    let prev = fibonacci(n-1)
+    prev.push(prev[prev.length-1] + prev[prev.length-2] )
+    return prev;
+}
+
+
+function deepDup(arr) {
+    if (!Array.isArray(arr)) {
+        return arr;
+    }
+    let dup = []
+    arr.forEach(ele => {
+        dup.push(deepDup(ele))
+    }
+
+    )
+    return dup
+}
